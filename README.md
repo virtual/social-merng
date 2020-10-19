1e3b1a1236a2000b369db3084c56c7cd31e1a784
Jeanine Schoessler <jeanine.mt@gmail.com>
2020-10-18 18:49:22 -0600

    add eslint and mongodb connection

---
+++ README.md
@@ -0,0 +1,10 @@
+# Social App
+
+A social app following the guide: [React / GraphQL Course - Build a social media app (MERNG Stack)](https://www.youtube.com/watch?v=n1mdAPFq2Os&ab_channel=freeCodeCamp.org)
+
+## Development
+
+Create a coy of `.env.sample` as `.env` and update the values
+
+- `npm install`
+- `npm start`
--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag')
+const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const mongodbUri = ('mongodb+srv://' 
+  + process.env.SERVER_USER+':'
+  + process.env.SERVER_PASSWORD 
+  + '@cluster0.2m67r.mongodb.net/merng?retryWrites=true&w=majority');
+
+  // Returns a promise, so use then for result obj
+mongoose.connect(mongodbUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log(`MongoDB Connected`);
+  return server.listen({port: 5000});
+})
+.then( res => {
+  console.log(`Server running at ${res.url}`);
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(`Mongoose connection error: ${err}`);
+  process.exit(1);
+});
+
 const typeDefs = gql`
   type Query {
     sayHi: String!
@@ -11,14 +36,9 @@ const resolvers = {
   Query: {
     sayHi: () => 'Hello, World!'
   }
-}
+};
 
 const server = new ApolloServer({
   typeDefs,
   resolvers
 });
-// Returns a promise, so use then for result obj
-server.listen({port: 5000})
-  .then( res => {
-    console.log(`Server running at ${res.url}`)
-  });
